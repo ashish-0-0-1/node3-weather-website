38cabae3dbd56fba29ca3fe5ffda4702de8a2f21
Ashish Chuabey <ashish.choubey@vivaconnect.co>
2022-08-04 12:06:00 +0530

    init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=4b948336fa32f7aca818d2e1a7b1dfcd&query='+ latitude + ',' + longitude
+
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.temperature + ' degress out. There is a ' + body.current.observation_time + 'its current time')
+        }
+    })
+}
+
+module.exports = forecast
