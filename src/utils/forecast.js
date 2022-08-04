d4715f5739a5ed186645ed381d2ea11479a81805
Ashish Chuabey <ashish.choubey@vivaconnect.co>
2022-08-04 13:35:21 +0530

    add about

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.temperature + ' degress out. There is a ' + body.current.observation_time + 'its current time')
+            callback(undefined, body.current.temperature + ' degress out. There is a ' + body.current.observation_time + 'its current time' +body.weather_descriptions.wind_speed+ 'is the wind speed right now and'+'the location comes under the country of'+body.location.country)
         }
     })
 }
